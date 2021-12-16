eb83c53b984df6db397f687bdf1d12571d1f8273
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-12-16 17:50:01 +0800

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+    return (
+        <Router basename={process.env.PUBLIC_URL}>
+            <div className="App">
+                <Navbar />
+            </div>
+        </Router>
+    );
 }
 
 export default App
