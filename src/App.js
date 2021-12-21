9c27d97cecc9b1bc04fbb942a38bebb8938396dd
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-12-21 17:05:02 +0800

    Add initial staking UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,86 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer';
+
+import ownlyLogo from './img/ownly/own-token.webp'
+import busdLogo from './img/busd/busd.webp'
 
 function App() {
     return (
         <Router basename={process.env.PUBLIC_URL}>
-            <div className="App">
+            <div className="app" style={{"backgroundColor": "rgb(244, 246, 248)"}}>
                 <Navbar />
+
+                <div className="container">
+                    <section id="app-staking" className="mb-4">
+                        <div className="row justify-content-center align-items-center">
+                            <div className="col-12 col-md-5">
+                                <p className="text-center font-size-170 text-color-3 neo-bold mb-1">Acquire OWN/BUSD LP Tokens</p>
+                                <p className="text-center font-size-90 text-color-6 neo-light mb-4">Add liquidity to OWN/BUSD on <b>PancakeSwap</b></p>
+                                <div className="d-flex justify-content-center mb-3">
+                                    <div style={{"width": "50px"}}>
+                                        <img src={ownlyLogo} className="w-100" alt="Ownly Logo" />
+                                    </div>
+                                    <div style={{"width": "50px"}}>
+                                        <img src={busdLogo} className="w-100" alt="BUSD Logo" />
+                                    </div>
+                                </div>
+                                <div className="mx-auto" style={{"width": "60%"}}>
+                                    <button className="w-100 btn btn-custom-7 rounded-lg">ADD LIQUIDITY FOR OWN/BUSD</button>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-7">
+                                <p className="text-center font-size-170 text-color-2 neo-bold mb-1">Stake OWN/BUSD LP Tokens</p>
+                                <p className="text-center font-size-90 text-color-6 neo-light mb-4">Stake your <b>CAKE LP Tokens</b> and receive <b>OWN</b></p>
+                                <p className="total-dep bg-color-4 text-white text-center font-size-110 text-color-6 neo-light mb-4"><b>TOTAL DEPOSITS:</b> 0.000000000000 OWN/BUSD</p>
+
+                                <div className="mx-auto" style={{"width": "85%"}}>
+                                    <div className="app-card">
+                                        <form>
+                                            <p className="font-size-110 neo-light mb-1">Stake</p>
+                                            <div className="form-group stake-form">
+                                                <input type="text" className="form-control form-control-lg stake-input" placeholder="Amount" />
+                                                {/* <small id="stake-help" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+
+                                                <button className="font-size-80 btn stake-btn neo-bold">MAX</button>
+                                            </div>
+                                            <div className="d-flex justify-content-between">
+                                                <button className="btn stake-btn-func btn-custom-2">APPROVE</button>
+                                                <button className="btn stake-btn-func btn-custom-2">STAKE</button>
+                                            </div>
+                                        </form>
+
+                                        <hr className="my-4" />
+
+                                        <div className="d-flex justify-content-between">
+                                            <p className="mb-3 neo-bold font-size-90">Total LP Tokens Staked</p>
+                                            <p className="mb-3 neo-regular font-size-90">0.000000000000 OWN/BUSD</p>
+                                        </div>
+                                        <div className="d-flex justify-content-between">
+                                            <p className="mb-3 neo-bold font-size-90">Rewards Earned</p>
+                                            <p className="mb-3 neo-regular font-size-90">0.000000000000 OWN</p>
+                                        </div>
+                                        <div className="d-flex justify-content-between">
+                                            <p className="mb-3 neo-bold font-size-90">APR</p>
+                                            <p className="mb-3 neo-regular font-size-90">-- %</p>
+                                        </div>
+                                        <div className="d-flex justify-content-between">
+                                            <p className="mb-3 neo-bold font-size-90">Rate</p>
+                                            <p className="mb-3 neo-regular font-size-90">0.000000000000 OWN / week</p>
+                                        </div>
+                                        <div className="d-flex justify-content-between">
+                                            <p className="mb-3 neo-bold font-size-90">Duration</p>
+                                            <p className="mb-3 neo-regular font-size-90">-- Days</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+                <Footer />
             </div>
         </Router>
     );
