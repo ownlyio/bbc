f5c3f4989bc7153bb99d2b12098d21399fa4f247
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-07-20 17:10:15 +0800

    Update og image and update urls

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
                 <div className="d-flex justify-content-center align-items-center h-full">
                     <div className="app-wrap">
                         <h1 className="app-title text-center font-size-280 font-size-md-350 font-size-lg-430 font-size-xl-500 text-white rubik-regular">BICOL BLOCKCHAIN CONFERENCE</h1>
-                        <p className="app-sub text-center font-size-150 font-size-md-180 font-size-lg-230 font-size-xl-300 text-white alegreya-regular mb-0">11.11.2022 | Legazpi, City</p>
+                        <p className="app-sub text-center font-size-150 font-size-md-180 font-size-lg-230 font-size-xl-300 text-white alegreya-regular mb-0">11.11.2022 | Legazpi City, Albay</p>
                     </div>
                 </div>
             </div>
