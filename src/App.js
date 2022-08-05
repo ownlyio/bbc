c67a8cf36108c4044bbd5e8cdff5f58226593dcd
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-08-05 10:20:45 +0800

    Update date of event

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
                 <div className="d-flex justify-content-center align-items-center h-full">
                     <div className="app-wrap">
                         <h1 className="app-title text-center font-size-280 font-size-md-350 font-size-lg-430 font-size-xl-470 text-white neo-bold">BICOL BLOCKCHAIN CONFERENCE</h1>
-                        <p className="app-sub text-center font-size-150 font-size-md-180 font-size-lg-230 font-size-xl-300 text-white alegreya-regular mb-5">11.11.2022 | Legazpi City, Albay</p>
+                        <p className="app-sub text-center font-size-150 font-size-md-180 font-size-lg-230 font-size-xl-300 text-white alegreya-regular mb-5">11.14.2022 | Legazpi City, Albay</p>
 
                         <div className="row justify-content-center">
                             <div className="col-lg-8 text-center">
