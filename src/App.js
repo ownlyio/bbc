a193e63895dc80e0797898087086c42ff570e154
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-08-17 15:54:07 +0800

    Add DTI ROV

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import axios from 'axios'
 import './App.css'
 
 import background from './img/bbc-banner.jpg'
+import dti5 from './img/dti-logo.png'
 
 function App() {
     const [emailAdd, setEmailAdd] = useState("")
@@ -38,12 +39,12 @@ function App() {
             <div className="container h-full">
                 <div className="d-flex justify-content-center align-items-center h-full">
                     <div className="app-wrap">
-                        <h1 className="app-title text-center font-size-280 font-size-md-350 font-size-lg-430 font-size-xl-470 text-white neo-bold">BICOL BLOCKCHAIN CONFERENCE</h1>
-                        <p className="app-sub text-center font-size-150 font-size-md-180 font-size-lg-230 font-size-xl-300 text-white alegreya-regular mb-5">11.14.2022 | Legazpi City, Albay</p>
+                        <h1 className="app-title text-center font-size-250 font-size-md-350 font-size-lg-430 font-size-xl-470 text-white neo-bold">BICOL BLOCKCHAIN CONFERENCE</h1>
+                        <p className="app-sub text-center font-size-150 font-size-md-180 font-size-lg-230 font-size-xl-300 text-white alegreya-regular mb-3 mb-sm-5">11.14.2022 | Legazpi City, Albay</p>
 
                         <div className="row justify-content-center">
                             <div className="col-lg-8 text-center">
-                                <p className="alegreya-regular text-white font-size-120 font-size-sm-130 text-center px-md-5 mb-4">Wanna be the first to get notified when we announce the details of the first-ever Bicol Blockchain Conference? Join our VIP List to be first to know.</p>
+                                <p className="alegreya-regular text-white font-size-100 font-size-sm-130 text-center px-md-5 mb-4">Wanna be the first to get notified when we announce the details of the first-ever Bicol Blockchain Conference? Join our VIP List to be first to know.</p>
                                 <div className="px-lg-5 mx-lg-5">
                                     <form className="app-bbm-form" onSubmit={submitForm}>
                                         <div className="d-flex align-items-center flex-wrap mb-2">
@@ -59,7 +60,7 @@ function App() {
                                                 <input className="form-check-input" type="checkbox" id="agreement" required />
                                             </div>
                                             <div className="ps-1">
-                                                <label className="form-check-label font-size-90 text-white" for="agreement">
+                                                <label className="form-check-label font-size-80 text-white" for="agreement">
                                                 By joining our Ownly VIP list, you agree to receive updates from Ownly for our upcoming IRL events. You can opt out of our marketing emails anytime. Your email will be stored on our database and will not be shared with any third-party.
                                                 </label>
                                             </div>
@@ -67,6 +68,14 @@ function App() {
                                     </form>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="mt-2 mt-sm-3 mt-lg-5">
+                            <p className="alegreya-regular text-center font-size-110 font-size-md-140 font-size-lg-180 font-size-xl-200 text-white mb-2">Co-presented by:</p>
+                            <div className="partner-img mx-auto">
+                                <img src={dti5} alt="DTI Region 5" className="w-100" />
+                            </div>
+                            <p className="alegreya-regular text-center font-size-90 font-size-md-110 font-size-lg-150 font-size-xl-170 text-white mb-2">Region 5</p>
                         </div>
                     </div>
                 </div>
