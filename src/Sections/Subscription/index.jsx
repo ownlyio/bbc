a5bb790f7d7d76b7af9c25b52ed373d825ca5b21
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-09-23 13:09:28 +0800

    Add subscrition section w/o input

---
+++ src/Sections/Subscription/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import { Text, Heading } from "../../components/Text";
+import PageLayout from "../../components/Layout";
+import { ContainerWithBackground } from "../../components/Layout/Container";
+import { Button } from "react-bootstrap";
+import useTheme from "../../hooks/useTheme";
+
+const Subscribe = () => {
+  const { theme } = useTheme()
+  return (
+    <PageLayout>
+      <ContainerWithBackground>
+        <div className="text-center">
+          <Heading fontSize="3em" color={theme.colors.secondary}>SUBSCRIBE FOR THE LATEST EVENT UPDATES</Heading>
+          <Text fontSize="1.5em">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+            tempora quos aut amet nobis. Nesciunt consequatur aperiam soluta
+            iste tempora eum doloribus nobis nulla, magni quibusdam velit
+            consequuntur culpa temporibus!
+          </Text>
+        </div>
+        <div className="d-flex align-items-center justify-content-center" style={{marginTop: '2em'}}>
+          <Button variant="outline"><Heading color={theme.colors.secondary}>Get Updates</Heading></Button>
+        </div>
+      </ContainerWithBackground>
+    </PageLayout>
+  );
+};
+
+export default Subscribe;
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Input = () => {
+  return (
+    <div>input</div>
+  )
+}
+
+export default Input;
