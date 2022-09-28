6866f424a05d38684fcccd2b153285bd48198101
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-09-28 14:59:05 +0800

    Change bg color

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0,0,0,0.6)
+  background-color: #252525;
 `
 
 const PageLoader = () => {
